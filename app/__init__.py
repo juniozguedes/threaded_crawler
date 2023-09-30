855ac030bec95106e5e8b2f977d3f4c4ce08e994
Wellington <juniozguedes@gmail.com>
2023-09-30 18:38:38 -0300

    Added: exceptions to csv module, csv output

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 import threading
 from flask import Flask
 
-# Extensions
 from app.config import Config as config_class
 from app.exceptions import CustomException, handle_custom_exception
 from app.extensions import ma
 
-# Scrappers
 from app.scrappers.company_csv.main import company_thread
 from app.scrappers.g2crowd.main import task3
 from app.scrappers.google_drive.main import task1
@@ -14,14 +12,14 @@ from app.scrappers.google_drive.main import task1
 app = Flask(__name__)
 app.config.from_object(config_class)
 
-# Register errors
 app.register_error_handler(CustomException, handle_custom_exception)
-# Initialize extensions here
 ma.init_app(app)
 
 print("Starting threads")
+CSV_INPUT = "companies_input.csv"
+
 thread1 = threading.Thread(target=task1)
-company_thread = threading.Thread(target=company_thread)
+company_thread = threading.Thread(target=company_thread(CSV_INPUT))
 thread3 = threading.Thread(target=task3)
 
 thread1.start()
--- app/scrappers/company_csv/main.py
+++ app/scrappers/company_csv/main.py
@@ -3,6 +3,8 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+from app.exceptions import CustomException, ValidationException
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -49,19 +51,36 @@ def read_csv_file(file_path):
             for row in csv_reader:
                 result.append(row[0])
             return result
-    except FileNotFoundError:
+    except FileNotFoundError as err:
         print(f"File not found: {file_path}")
-        return False
+        message = err.args[1]
+        raise ValidationException(validation_message=message) from err
     except Exception as exception:
         print(f"An error occurred: {exception}")
-        return False
+        raise CustomException("An error occurred", 500) from exception
 
 
-def company_thread():
+def company_thread(csv_input):
     print("Running company csv thread")
-    companies_list = read_csv_file("companies.csv")
-    links = []
+    companies_list = read_csv_file(csv_input)
+    linkedin_urls = []
     for company in companies_list:
         company_linkedin = google_search(company)
-        links.append(company_linkedin)
-    return links
+        linkedin_urls.append(company_linkedin)
+
+    # Specify the CSV file path
+    csv_file_path = "urls.csv"
+
+    # Open the CSV file in write mode
+    with open(csv_file_path, mode="w", newline="", encoding="utf-8") as csv_file:
+        # Create a CSV writer
+        csv_writer = csv.writer(csv_file)
+
+        # Write the header row if needed (e.g., 'URLs')
+        # csv_writer.writerow(['URLs'])
+
+        # Write each URL to a new row in the CSV file
+        for url in linkedin_urls:
+            csv_writer.writerow([url])
+
+    print(f"URLs have been saved to {csv_file_path}")
