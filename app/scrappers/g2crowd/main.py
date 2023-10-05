81b4364a74bdc320dcfd0e7a9a6932533a3ac58e
Wellington <juniozguedes@gmail.com>
2023-10-05 17:31:08 -0300

    Scrapped reviews header data

--- app/scrappers/g2crowd/main.py
+++ app/scrappers/g2crowd/main.py
@@ -1,3 +1,5 @@
+# pylint: disable=R0914,R0915
+
 import os
 import time
 from playwright.sync_api import sync_playwright, Page
@@ -8,22 +10,22 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_review_data(page: Page, g2crowd_url: str):
-    time.sleep(3)
     page.goto(g2crowd_url)
-    time.sleep(5)
     frame_one = page.wait_for_selector("iframe").content_frame()
-    time.sleep(2)
     span = frame_one.wait_for_selector("input")
     if span.is_visible():
         span.click()
-        time.sleep(3)
-    review_data = {}
+        time.sleep(2)
+    review_data = {"reviews": []}
+
+    # PRODUCT PART ###
     product_name_tree = page.wait_for_selector("div.product-head__title")
-    # Execute JavaScript to get the inner text of the first <a> tag within product_name
     product_name = page.evaluate(
         "(element) => element.querySelector('a').innerText", product_name_tree
     )
-    print(product_name)
+    review_data["product_name"] = product_name
+
+    # PRICING PART ###
     pricing_cards = page.query_selector_all("a.preview-cards__card")
     if pricing_cards[0].is_visible():
         for a_tag in pricing_cards:
@@ -48,8 +50,7 @@ def get_review_data(page: Page, g2crowd_url: str):
             pricing["billing_text"] = extracted_data["billingText"]
             review_data["pricing"] = pricing
 
-    # Rating part
-    # Locate the element by its CSS selector
+    # RATING PART ###
     rating_element = page.query_selector("span.c-midnight-90.pl-4th")
 
     if rating_element:
@@ -61,11 +62,54 @@ def get_review_data(page: Page, g2crowd_url: str):
             rating_element,
         )
 
-        # Now, complete_rating contains the extracted complete rating (e.g., "4.3 out of 5")
         print("Complete Rating:", complete_rating)
+        review_data["rating"] = rating_element
+
+    # USER REVIEWS PART ###
+    itemtype = "http://schema.org/Review"
+    itemprop = "review"
+
+    # Use Playwright's locator to find the element
+    review_elements = page.locator(
+        f'//div[@itemtype="{itemtype}" and @itemprop="{itemprop}"]'
+    )
+    count = review_elements.count()
+    if review_elements:
+        for i in range(count):
+            review = {}
+            review_element = review_elements.nth(i)
+
+            # Reviewer name
+            link_locator = review_element.locator(".link--header-color")
+
+            if link_locator.count() > 0:
+                # Get the content inside the <a> tag
+                reviewer_name = link_locator.inner_text()
+
+            # Reviewer role and company
+            reviewer_info_locator = review_element.locator(
+                ".c-midnight-80.line-height-h6.fw-regular"
+            )
+            reviewer_info_elements = reviewer_info_locator.all()
+            if reviewer_info_elements:
+                reviewer_info_element = reviewer_info_elements[0]
+                reviewer_info = reviewer_info_element.inner_text().strip()
+                company_start = reviewer_info.find(
+                    "("
+                )  # Find the start of the company information
+                if company_start != -1:
+                    raw_reviewer_info = reviewer_info[:company_start].strip()
+                    role, company_size = raw_reviewer_info.split("\n")
+                    review["role"] = role
+                    review["company_size"] = company_size
+                    review["employeers"] = reviewer_info[company_start:].strip()
+
+            review["reviewer_name"] = reviewer_name
+            review_data["reviews"].append(review)
+
     else:
-        print("Rating element not found")
-    return "NA"
+        print("No review elements found")
+    return review_data
 
 
 def g2crowd_scrapper(url: str):
@@ -78,8 +122,7 @@ def g2crowd_scrapper(url: str):
 
         time.sleep(1)
         review_data = get_review_data(page, url)
-        print(review_data)
-        return True
+        return review_data
 
 
 def crowd2url_thread():
