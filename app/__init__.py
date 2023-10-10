c9c8bee442c975be6818186c783c646d000cf87e
Wellington <juniozguedes@gmail.com>
2023-10-09 22:01:39 -0300

    Added logs

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 
 from app.config import Config as config_class, start_logger
 from app.exceptions import CustomException, handle_custom_exception
-
 from app.scrappers.company_csv.main import company_orchestrator
 from app.scrappers.g2crowd.main import g2crowd_orchestrator
+from app.scrappers.google_drive.main import google_drive_orchestrator
 
 
 app = Flask(__name__)
@@ -25,6 +25,13 @@ company_thread = threading.Thread(
     name="CompanyThread",
 )
 crowd2url_thread = threading.Thread(target=g2crowd_orchestrator, name="Crowd2UrlThread")
-
+# Define your natural language query
+NATURAL_QUERY = "FILENAME Currículo, TEXT Guedes"
+google_drive_thread = threading.Thread(
+    target=google_drive_orchestrator,
+    args=(NATURAL_QUERY,),
+    name="GoogleDriveThread",
+)
 company_thread.start()
 crowd2url_thread.start()
+google_drive_thread.start()
--- app/scrappers/google_drive/main.py
+++ app/scrappers/google_drive/main.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import os.path
-
+import logging
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -11,11 +11,36 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
+# Get the directory where main.py is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+credentials_input = os.path.join(script_dir, "credentials.json")
 
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
+
+def parse_natural_language_query(natural_query):
+    logging.info("Parsing natural language query %s", natural_query)
+    # Split the query into parts
+    parts = natural_query.split(", ")
+
+    parsed_query = {"FILENAME": None, "TEXT": None}
+
+    for part in parts:
+        # Split each part into key and value
+        try:
+            key, value = part.split(" ", 1)
+            key = key.upper()  # Convert key to uppercase
+            if key in parsed_query:
+                parsed_query[key] = value
+            else:
+                raise ValueError(f"Invalid keyword: {key}")
+        except ValueError as value_error:
+            # Handle the case where the split didn't result in exactly two values
+            raise ValueError(f"Invalid query part: {part}") from value_error
+    logging.info("Parsed query: %s", parsed_query)
+    return parsed_query
+
+
+def google_credentials():
+    logging.info("Getting google_drive credentials from credentials.json")
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -27,43 +52,61 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_input, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open("token.json", "w", encoding="utf-8") as token:
             token.write(creds.to_json())
+    return creds
 
+
+def get_driver_items(creds, parsed_query):
     try:
+        logging.info("Running get_driver_items to SELECT documents")
         service = build("drive", "v3", credentials=creds)
 
-        # Call the Drive v3 API
+        filename_query = (
+            f"name contains '{parsed_query['FILENAME']}'"
+            if parsed_query["FILENAME"]
+            else ""
+        )
+        text_query = (
+            f"fullText contains '{parsed_query['TEXT']}'"
+            if parsed_query["TEXT"]
+            else ""
+        )
+
+        # Construct the final query
+        final_query = " and ".join(filter(None, [filename_query, text_query]))
         results = (
             service.files()
-            .list(pageSize=10, fields="nextPageToken, files(id, name)")
+            .list(pageSize=10, fields="nextPageToken, files(id, name)", q=final_query)
             .execute()
         )
         items = results.get("files", [])
 
         if not items:
-            print("No files found.")
-            return
-        print("Files:")
-        for item in items:
-            print("%s (%s)", item["name"], item["id"])
+            logging.info("No files found")
+        logging.info("Items found on driver: %s", items)
+        return items
+
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
         print(f"An error occurred: {error}")
+        return None
 
 
-if __name__ == "__main__":
-    main()
-
-
-def google_drive_orchestrator(query: str):
+def google_drive_orchestrator(natural_query: str):
     """
-    This function will call the orchestrator that queries google_drive
+    This function will call google drive API providing natural language queries:
+    Example: "FILENAME Resumee, TEXT python experience"
+    Explanation: In the example above, we search for a file with filename "Resumee" and inside text
+    containing "python experience"
     Output: query print/log
     return: query print/log
     """
-    print(query)
-    return True
+    logging.info("Running google_drive thread")
+    creds = google_credentials()
+    parsed_query = parse_natural_language_query(natural_query)
+    items = get_driver_items(creds, parsed_query)
+    return items
